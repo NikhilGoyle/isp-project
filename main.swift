843f80ffb118b90765ead80a0c00e8dc04b1ac9b
Nikhil Goyle <nikhil.goyle@student.allenisd.org>
2019-04-03 22:27:15 -0500

    created fluid swapping between modes

--- main.swift
+++ main.swift
@@ -30,20 +30,38 @@ let cyan = Color.standard(.cyan)
 let cyanOnBlue = colors.newPair(foreground:cyan, background:blue)
 mainWindow.turnOn(cyanOnBlue)
 
-func colorSwitch(old:ColorPair, new:ColorPair) {
-    mainWindow.turnOff(old)
-    mainWindow.turnOn(new)
-    }
+let red = Color.standard(.red)
+let yellow = Color.standard(.yellow)
+let redOnYellow = colors.newPair(foreground:red, background:yellow)
+
+
 
 var currentMap = maps.tutorial1
+var backup = maps.tutorial1
+var backupCoordinates = Point(x:0,y:0)
 var mapNumber = 1
 var mapPosition = Point(x:2,y:2)
 var cursorPosition = Point(x:4,y:4)
 
+
+/*for rows in maps.loopTest {
+    for cell in rows {
+        for i in stride(from:0,to:10,by:1) {
+            if Int.random(in:0...1) == 0 {            
+                maps.loopTest.append([0])
+            } else {
+                maps.loopTest.append([1])
+            }
+        }
+    }
+    }*/
+
+cursor.position = Point(x:5,y:5) //initialize position
 func mapBuild(plan: [[Int]]) {
+    cursorPosition = cursor.position
     mainWindow.clear()
-    let locationX = 2
-    var locationY = 2
+    let locationX = 3
+    var locationY = 3
     cursor.position = Point(x:locationX, y:locationY)
     for rows in plan {
         for cell in rows {
@@ -62,17 +80,27 @@ func mapBuild(plan: [[Int]]) {
     mainWindow.refresh()
 }
 
+func colorSwitch(old:ColorPair, new:ColorPair) {
+    mainWindow.turnOff(old)
+    mainWindow.turnOn(new)
+    mapBuild(plan:currentMap)
+    mainWindow.refresh()
+}
+
 func mapSelect(number: Int)-> [[Int]] {
     switch (number) {
     case 1: mapPosition = Point(x:2,y:2)
-            cursorPosition = Point(x:4,y:4)
+            cursorPosition = Point(x:5,y:5)
             return maps.tutorial1
     case 2: mapPosition = Point(x:2,y:2)
-            cursorPosition = Point(x:4,y:4)
+            cursorPosition = Point(x:5,y:5)
             return maps.tutorial2
+    case 3: mapPosition = Point(x:5,y:1)
+            cursorPosition = Point(x:8,y:4)
+            return maps.lava1
     default:
         mapPosition = Point(x:2,y:2)
-        cursor.position = Point(x:4,y:4)
+        cursor.position = Point(x:5,y:5)
         return maps.tutorial1
     }
 }
@@ -81,6 +109,7 @@ mapBuild(plan:mapSelect(number:mapNumber))
 
 var standard = true
 var wallbreaker = false
+var lavafloor = false
 
 while true {
     while standard {
@@ -139,8 +168,12 @@ while true {
                 mainWindow.refresh()
             }
         default: do {
-                     if key.character == "e" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                     if key.character == "2" && currentMap[mapPosition.y][mapPosition.x] != 0 {
                          wallbreaker = true
+                         standard = false
+                     }
+                     if key.character == "3" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                         lavafloor = true
                          standard = false
                      }
                  }//do
@@ -179,8 +212,12 @@ while true {
                 mainWindow.refresh()
             }
         default: do {
-                     if key.character == "e" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                     if key.character == "1" && currentMap[mapPosition.y][mapPosition.x] != 0 {
                          standard = true
+                         wallbreaker = false
+                     }
+                     if key.character == "3" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                         lavafloor = true
                          wallbreaker = false
                      }
                      if key.character == "w" {
@@ -234,5 +271,70 @@ while true {
                  }//default
         }//switchcase
     }//wallbreaker
+    while lavafloor {
+        colorSwitch(old:cyanOnBlue,new:redOnYellow)
+        mapBuild(plan:currentMap)
+        maps.backup = currentMap
+        backupCoordinates = cursor.position
+        let key = keyboard.getKey(window:mainWindow)
+        switch (key.keyType) {
+        case .arrowUp:
+            if currentMap[mapPosition.y-1][mapPosition.x] == 1 {
+                currentMap[mapPosition.y][mapPosition.x] = 0
+                cursor.position = Point(x:cursor.position.x, y:cursor.position.y-1)
+                mapPosition = Point(x:mapPosition.x, y:mapPosition.y-1)
+                mainWindow.refresh()
+            }
+            else if currentMap[mapPosition.y-1][mapPosition.x] == 0 {
+                mapBuild(plan:maps.backup)//should be correct (but it isnt)
+                cursor.position = backupCoordinates
+            }
+        case .arrowDown:
+            if currentMap[mapPosition.y+1][mapPosition.x] == 1 {
+                currentMap[mapPosition.y][mapPosition.x] = 0
+                cursor.position = Point(x:cursor.position.x, y:cursor.position.y+1)
+                mapPosition = Point(x:mapPosition.x, y:mapPosition.y+1)
+                mainWindow.refresh()
+            }
+            else if currentMap[mapPosition.y+1][mapPosition.x] == 0 {
+                mapBuild(plan:backup)
+                cursor.position = backupCoordinates
+            }
+        case .arrowLeft:
+            if currentMap[mapPosition.y][mapPosition.x-1] == 1 {
+                currentMap[mapPosition.y][mapPosition.x] = 0
+                cursor.position = Point(x:cursor.position.x-1, y:cursor.position.y)
+                mapPosition = Point(x:mapPosition.x-1, y:mapPosition.y)
+                mainWindow.refresh()
+            }
+            else if currentMap[mapPosition.y-1][mapPosition.x] == 0 {
+                mapBuild(plan:backup)
+                cursor.position = backupCoordinates
+            }
+        case .arrowRight:
+            if currentMap[mapPosition.y][mapPosition.x+1] == 1 {
+                currentMap[mapPosition.y][mapPosition.x] = 0
+                cursor.position = Point(x:cursor.position.x+1, y:cursor.position.y)
+                mapPosition = Point(x:mapPosition.x+1, y:mapPosition.y)
+                mainWindow.refresh()
+            }
+            else if currentMap[mapPosition.y-1][mapPosition.x] == 0 {
+                mapBuild(plan:backup)
+                cursor.position = backupCoordinates
+            }
+        default: do {
+                     if key.character == "1" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                         colorSwitch(old:redOnYellow,new:cyanOnBlue)
+                         standard = true
+                         lavafloor = false
+                     }
+                     if key.character == "2" && currentMap[mapPosition.y][mapPosition.x] != 0 {
+                         colorSwitch(old:redOnYellow,new:cyanOnBlue)
+                         wallbreaker = true
+                         lavafloor = false
+                     }
+                 }
+        }//switchcase
+    }//while lavafloor
 }//big true
 
